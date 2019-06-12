59289168e0a29844bc5795089ebbdf1a948888a8
hdijkema <hans@dykema.nl>
2019-06-12 13:46:20 +0200

    Batch mode created.
    Signed-off-by: hdijkema <hans@dykema.nl>

--- GetStarted/MainWindow.xaml.cs
+++ GetStarted/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using MyScript.IInk.UIReferenceImplementation;
+using MyScriptRecognizer;
 
 namespace MyScript.IInk.GetStarted
 {
@@ -16,10 +17,15 @@ namespace MyScript.IInk.GetStarted
     public partial class MainWindow : Window
     {
         // Defines the type of content (possible values are: "Text Document", "Text", "Diagram", "Math", and "Drawing")
-        private const string PART_TYPE = "Text Document";
+        private const string PART_TYPE = "Text";
 
         private Engine _engine;
-        private Editor _editor => UcEditor.Editor;
+        private Editor _editor; // => UcEditor.Editor;
+        private zcRecognizer _recognizer;
+        private float _dpiX, _dpiY;
+        private Renderer _renderer;
+        private ContentPackage _package;
+        private ContentPart _part;
 
         public MainWindow()
         {
@@ -47,12 +53,47 @@ namespace MyScript.IInk.GetStarted
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            try
-            {
+            var localFolder = "c:\\devel\\recognize"; // Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var tempFolder = localFolder + "\\tmp"; //Path.Combine(localFolder, "MyScript", "tmp");
+
+            /*try
+             {
+                 // Initialize Interactive Ink runtime environment
+                 _engine = Engine.Create(MyScript.Certificate.MyCertificate.Bytes);
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                 Close();
+                 return;
+             }
+
+             // Folders "conf" and "resources" are currently parts of the layout
+             // (for each conf/res file of the project => properties => "Build Action = content")
+             string[] confDirs = new string[1];
+             confDirs[0] = "conf";
+             _engine.Configuration.SetStringArray("configuration-manager.search-path", confDirs);
+
+             _engine.Configuration.SetString("content-package.temp-folder", tempFolder);
+
+             // Initialize the editor with the engine
+             UcEditor.Engine = _engine;
+             UcEditor.Initialize(this);
+
+             ContentPackage package = _engine.CreatePackage("text.iink");
+             ContentPart part = package.CreatePart("Text");
+             UcEditor.Editor.Part = part;
+
+             _recognizer = new zcRecognizer(localFolder);
+             Renderer r = UcEditor.Renderer;
+             _recognizer.initFromVars(_engine, UcEditor.Editor, r, r.DpiX, r.DpiY, package, part);
+             */
+             /*
+            try {
                 // Initialize Interactive Ink runtime environment
                 _engine = Engine.Create(MyScript.Certificate.MyCertificate.Bytes);
             }
-            catch (Exception ex)
+             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 Close();
@@ -65,13 +106,45 @@ namespace MyScript.IInk.GetStarted
             confDirs[0] = "conf";
             _engine.Configuration.SetStringArray("configuration-manager.search-path", confDirs);
 
-            var localFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var tempFolder =  Path.Combine(localFolder, "MyScript", "tmp");
             _engine.Configuration.SetString("content-package.temp-folder", tempFolder);
 
+            //_engine.Configuration.SetString("lang", "nl_NL");
+
             // Initialize the editor with the engine
+
+            _dpiX = 300;
+            _dpiY = 300;
+            _renderer = _engine.CreateRenderer(_dpiX, _dpiY, null);
+            _package = _engine.CreatePackage("text.iink");
+            _part = _package.CreatePart("Text");
+
+            _editor = _engine.CreateEditor(_renderer);
+            _editor.Theme = ".text { font-size: 7.8;line-height: 1.0; }";
+            _editor.SetViewSize(30000, 30000);
+            var fmp = new FontMetricsProvider(_dpiX, _dpiY);
+            _editor.SetFontMetricsProvider(fmp);
+
+            _editor.Part = _part;
+            */
+            /*
             UcEditor.Engine = _engine;
             UcEditor.Initialize(this);
+
+            ContentPackage package = _engine.CreatePackage("text.iink");
+            ContentPart part = package.CreatePart("Text");
+            UcEditor.Editor.Part = part;
+
+            _recognizer = new zcRecognizer(localFolder);
+            Renderer r = UcEditor.Renderer;
+            */
+
+            /*Renderer r = _renderer;
+            _recognizer = new zcRecognizer(localFolder);
+            _recognizer.initFromVars(_engine, _editor, r, r.DpiX, r.DpiY, _package, _part);
+            */
+
+            _recognizer = new zcRecognizer(localFolder);
+            _recognizer.initDefault();
 
             // Force pointer to be a pen, for an automatic detection, set InputMode to AUTO
             SetInputMode(InputMode.PEN);
@@ -81,22 +154,22 @@ namespace MyScript.IInk.GetStarted
 
         private void EditUndo_MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            _editor.Undo();
+            //_editor.Undo();
         }
 
         private void EditRedo_MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            _editor.Redo();
+            //_editor.Redo();
         }
 
         private void EditClear_MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            _editor.Clear();
+            //_editor.Clear();
         }
 
         private void EditConvert_MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            try
+            /*try
             {
                 var supportedStates = _editor.GetSupportedTargetConversionStates(null);
 
@@ -106,11 +179,21 @@ namespace MyScript.IInk.GetStarted
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }*/
+
+            try
+            {
+                _recognizer.Convert();
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ToString());
             }
         }
 
         public void NewFile()
         {
+            /*
             // Close current package
             if (_editor.Part != null)
             {
@@ -129,10 +212,12 @@ namespace MyScript.IInk.GetStarted
                 _editor.Part = part;
                 Type.Text = "Type: " + PART_TYPE;
             }
+            */
         }
 
         private string MakeUntitledFilename()
         {
+            /*
             var localFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             int num = 0;
             string name;
@@ -145,44 +230,54 @@ namespace MyScript.IInk.GetStarted
             while (System.IO.File.Exists(name));
 
             return name;
+            */
+            return "";
         }
 
         private void SetInputMode(InputMode inputMode)
         {
+            /*
             UcEditor.InputMode = inputMode;
             Auto.IsChecked = (inputMode == InputMode.AUTO);
             Touch.IsChecked = (inputMode == InputMode.TOUCH);
             Pen.IsChecked = (inputMode == InputMode.PEN);
+            */
         }
 
         private void Pen_Click(object sender, RoutedEventArgs e)
         {
+            /*
             ToggleButton toggleButton = sender as ToggleButton;
 
             if ((bool)toggleButton.IsChecked)
             {
                 SetInputMode(InputMode.PEN);
             }
+            */
         }
 
         private void Touch_Click(object sender, RoutedEventArgs e)
         {
+            /*
             ToggleButton toggleButton = sender as ToggleButton;
 
             if ((bool)toggleButton.IsChecked)
             {
                 SetInputMode(InputMode.TOUCH);
             }
+            */
         }
 
         private void Auto_Click(object sender, RoutedEventArgs e)
         {
+            /*
             ToggleButton toggleButton = sender as ToggleButton;
 
             if ((bool)toggleButton.IsChecked)
             {
                 SetInputMode(InputMode.AUTO);
             }
+            */
         }
     }
 }
---
+++ GetStarted/zcRecognizer.cs
@@ -0,0 +1,249 @@
+﻿using MyScript.IInk;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using MyScript.IInk.UIReferenceImplementation;
+
+[DataContract]
+internal class Cmd
+{
+    [DataMember(Name = "cmd", IsRequired = true)]
+    internal String cmd { get; set; } = "";
+    [DataMember(Name = "input", IsRequired = false)]
+    internal String input_file { get; set; } = "";
+    [DataMember(Name = "result", IsRequired = false)]
+    internal String output_file { get; set; } = "";
+}
+
+[DataContract]
+internal class StrokePoint
+{
+    [DataMember(Name = "x", IsRequired = true)]
+    internal float[] x { get; set; } = null;
+    [DataMember(Name = "y", IsRequired = true)]
+    internal float[] y { get; set; } = null;
+}
+
+[DataContract]
+internal class StrokesInput
+{
+    [DataMember(Name = "type", IsRequired = true)]
+    internal String type { get; set; } = "";
+    [DataMember(Name = "strokes", IsRequired = true)]
+    internal StrokePoint[] strokes { get; set; } = null;
+}
+
+[DataContract]
+internal class Result
+{
+    [DataMember(Name = "result_type", IsRequired = true)]
+    internal string type { get; set; } = "";
+    [DataMember(Name = "result", IsRequired = true)]
+    internal String result { get; set; } = "";
+}
+
+internal class Listener : IEditorListener
+{
+    public void ContentChanged(Editor editor, string[] blockIds)
+    {
+        String ids = "";
+        String comma  = "";
+        int i, N;
+        for(i = 0, N = blockIds.Length; i < N; i++)
+        {
+            ids += comma;
+            ids += blockIds[i];
+            comma = ", ";
+        }
+        Console.WriteLine("Content Changed: " + ids);
+    }
+
+    public void OnError(Editor editor, string blockId, string message)
+    {
+        Console.WriteLine("Error(" + blockId + "): " + message);
+    }
+
+    public void PartChanged(Editor editor)
+    {
+        Console.WriteLine("Part Changed");
+    }
+}
+
+namespace MyScriptRecognizer
+{
+    public class zcRecognizer 
+    {
+        String _path;
+        String _tmp_path;
+
+        Editor _editor;
+        Engine _engine;
+        Renderer _renderer;
+        ContentPackage _package;
+        ContentPart _part;
+
+        float _dpiX;
+        float _dpiY;
+
+        private String mkFileName(String basedir, String name)
+        {
+            return basedir + "\\" + name;
+        }
+
+        public zcRecognizer(String _monitor_directory)
+        {
+            _path = _monitor_directory;
+            _tmp_path = _path + "\\" + "tmp";
+            DirectoryInfo d = new DirectoryInfo(_tmp_path);
+            d.Create();
+				}
+				
+	    public void initDefault()
+		{
+            _engine = Engine.Create(MyScript.Certificate.MyCertificate.Bytes);
+            var confDirs = new string[1];
+            confDirs[0] = "conf";
+            _engine.Configuration.SetStringArray("configuration-manager.search-path", confDirs);
+
+            var localFolder = _path;
+            var tempFolder = _tmp_path;
+            _engine.Configuration.SetString("content-package.temp-folder", tempFolder);
+
+            _engine.Configuration.SetString("lang", "nl_NL");
+
+            // Initialize the editor with the engine
+            //UcEditor.Engine = _engine;
+            _dpiX = 300;
+            _dpiY = 300;
+            _renderer = _engine.CreateRenderer(_dpiX, _dpiY, null);
+            _package = _engine.CreatePackage("text.iink");
+            _part = _package.CreatePart("Text");
+
+            _editor = _engine.CreateEditor(_renderer);
+            _editor.Theme = ".text { font-size: 7.8;line-height: 1.0; }";
+            _editor.SetViewSize(30000, 30000);
+            var fmp = new FontMetricsProvider(_dpiX, _dpiY);
+            _editor.SetFontMetricsProvider(fmp);
+
+            _editor.Part = _part;
+
+            initListener();
+        }
+				
+		public void initFromVars(Engine e, Editor ed, Renderer r, float dpix, float dpiy, ContentPackage pk, ContentPart part)
+		{
+			_dpiX = dpix;
+			_dpiY = dpiy;
+			_engine = e;
+			_editor = ed;
+			_part = part;
+			_package = pk;
+			_renderer = r;
+            initListener();
+		}
+
+        private void initListener()
+        {
+            _editor.AddListener(new Listener());
+        }
+
+        public bool Convert()
+        {
+            float dpix = _dpiX;
+            float dpiy = _dpiY;
+            Editor editor = _editor;
+
+            var cmd_file = _path + "/cmd.json";
+            cmd_file = cmd_file.Replace('/', '\\');
+            FileInfo cmd_f = new FileInfo(cmd_file);
+            if (cmd_f.Exists)
+            {
+                FileStream f_in = cmd_f.OpenRead();
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Cmd));
+                Cmd c = (Cmd)ser.ReadObject(f_in);
+                f_in.Dispose();
+
+                if (c.cmd == "quit")
+                {
+                    cmd_f.Delete();
+                    return true;    // Quit
+                }
+                else if (c.cmd == "recognize")
+                {
+                    editor.Clear();
+
+                    var input_file = c.input_file;
+                    FileInfo in_f = new FileInfo(input_file);
+                    DataContractJsonSerializer rec_ser = new DataContractJsonSerializer(typeof(StrokesInput));
+                    FileStream in_f_s = in_f.OpenRead();
+                    StrokesInput si = (StrokesInput)rec_ser.ReadObject(in_f_s);
+                    in_f_s.Dispose();
+
+                    Debug.WriteLine("Recognize received");
+
+                    List<PointerEvent> events = new List<PointerEvent>();
+
+                    int i, N;
+                    for(i = 0, N = si.strokes.Length;i < N; i++)
+                    {
+                        var stroke = si.strokes[i];
+                        int j, M;
+                        for(j = 0, M = stroke.x.Length; j < M; j++)
+                        {
+                            float x = stroke.x[j] / (300.0f / dpix);
+                            float y = stroke.y[j] / (300.0f / dpiy);
+
+                            PointerEvent evt;
+
+                            if (j == 0 && j == M - 1)
+                            {
+                                evt = new PointerEvent().Down(x, y);
+                                events.Add(evt);
+                                evt = new PointerEvent().Up(x, y);
+                            }
+                            else if (j == 0)
+                            {
+                                evt = new PointerEvent().Down(x, y);
+                            } else if (j == M - 1)
+                            {
+                                evt = new PointerEvent().Up(x, y);
+                            } else
+                            {
+                                evt = new PointerEvent().Move(x, y);
+                            }
+
+                            events.Add(evt);
+                        }
+                    }
+
+                    editor.PointerEvents(events.ToArray(), false);
+                            
+                    editor.WaitForIdle();
+                    String result = editor.Export_(editor.GetRootBlock(), MimeType.TEXT);
+
+                    in_f.Delete();
+
+                    var result_file = c.output_file + "_ff";
+                    var result_ren = c.output_file;
+                    FileInfo result_f = new FileInfo(result_file);
+                    FileStream f_out = result_f.OpenWrite();
+                    Result r = new Result();
+                    r.type = "text";
+                    r.result = result;
+                    DataContractJsonSerializer res_ser = new DataContractJsonSerializer(typeof(Result));
+                    res_ser.WriteObject(f_out, r);
+                    f_out.Dispose();
+                    result_f.MoveTo(result_ren);
+                }
+
+                cmd_f.Delete();
+            }
+
+            return false; // Don't quit
+        }
+    }
+}
